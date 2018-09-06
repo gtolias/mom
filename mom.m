35ef1794b4fd66300ec4627f19a0fa76f4b8b311
G Tolias <gtolias@image.ntua.gr>
2018-09-06 14:05:57 +0200

    save examples using loaded thumbnails

--- mom.m
+++ mom.m
@@ -12,21 +12,19 @@ if ~exist('vgg_rmac_1M_mom.mat')
 	system('wget http://cmp.felk.cvut.cz/~toliageo/ext/mom/vgg_rmac_1M_mom.mat');
 	load('vgg_rmac_1M_mom.mat');
 end
-datetime
 
 % Na = Inf;   % all images used as anchors: setup for fine-grained recognition in CVPR18
 Na = 1500;  % choose based on random walk on graph: setup for retrieval in CVPR18
 
-fprintf('kNN graph construction\n');	
+fprintf('kNN graph construction (it will take few hours)\n');	
 [knn, s] = knn_wrap(V, V, 30); % 30 nearest neighbors used in CVPR18. x3 faster if yael_nn available
 G = knngraph(knn, s .^ 3); 		 % similarity^3 as in CVPR18 and CVPR17
-datetime
 
 % seed selection
 fprintf('seed select \n');	
 ids = 1:size(V,2);
 
-if isinf(Na)
+if ~isinf(Na)
 	cc = largecc(G, 1, 'rank'); 	% keep only the biggest connected component
 	G = G(cc, cc);
 	V = V(:, cc);
@@ -42,7 +40,6 @@ if isinf(Na)
 else
 	anc_idx = 1:size(G, 2);
 end
-datetime
 
 fprintf('pool select \n');	
 L = speye(size(G)) - 0.99 * transition_matrix(G); % Laplacian, alpha = 0.99 used in CVPR18
@@ -50,14 +47,70 @@ L = speye(size(G)) - 0.99 * transition_matrix(G); % Laplacian, alpha = 0.99 used
 k = 50;  % parameter k in CVPR18, equation (6)
 maxpoolsize = 50;  % keep not more than
 [pos, prest] = posmine(V, anc_idx, L, k, maxpoolsize); 
-datetime
 
 k = 10000; % parameter k in CVPR18, equation (6)
 maxpoolsize = 50;  % keep not more than
 [neg, nrest] = negmine(V, anc_idx, L, k, maxpoolsize);
-datetime
 
 % map ids back to the original ones
 anc_idx  = ids(anc_idx);
 pos  = cellfun(@(x) ids(x), pos, 'un', 0);
 neg  = cellfun(@(x) ids(x), neg, 'un', 0);
+
+
+return; % comment out to show examples of the selection
+
+% show examples from the constructed training set
+fprintf('Save some examples on disk\n');
+
+% load image thumbnails (used only to show some examples)
+if ~exist('img_1M_thumbgray.mat.mat')
+	% download and load the input descriptors (extracted with vgg-imagenet-rmac)
+	system('wget http://cmp.felk.cvut.cz/~toliageo/ext/mom/img_1M_thumbgray.mat');
+	load('img_1M_thumbgray.mat');
+end
+
+% show some of the chosen anchors (meaningful only in the case of random-walk)
+h = figure('Visible', 'off');
+for i = 1:25, subplot(5,5, i), imshow(img{anc_idx(i)}); end
+saveas(h, 'top25_anchors.png');
+h = figure('Visible', 'off');
+for i = 1:25, subplot(5,5, i), imshow(img{anc_idx(500+i)}); end
+saveas(h, 'top501-525_anchors.png');
+h = figure('Visible', 'off');
+for i = 1:25, subplot(5,5, i), imshow(img{anc_idx(1000+i)}); end
+saveas(h, 'top1001-1025_anchors.png');
+
+% show some positives
+h = figure('Visible', 'off');
+subplot(3,3,1)
+imshow(img{anc_idx(1)});
+for i = 1:min(8,numel(pos{1})), subplot(3,3,i+1); imshow(img{pos{1}(i)}); end
+saveas(h, 'pos_anchor1.png');
+h = figure('Visible', 'off');
+subplot(3,3,1)
+imshow(img{anc_idx(30)});
+for i = 1:min(8,numel(pos{30})), subplot(3,3,i+1); imshow(img{pos{30}(i)}); end
+saveas(h, 'pos_anchor30.png');
+h = figure('Visible', 'off');
+subplot(3,3,1)
+imshow(img{anc_idx(100)});
+for i = 1:min(8,numel(pos{100})), subplot(3,3,i+1); imshow(img{pos{100}(i)}); end
+saveas(h, 'pos_anchor100.png');
+
+% show some negatives
+h = figure('Visible', 'off');
+subplot(3,3,1)
+imshow(img{anc_idx(1)});
+for i = 1:min(8,numel(neg{1})), subplot(3,3,i+1); imshow(img{neg{1}(i)}); end
+saveas(h, 'neg_anchor1.png');
+h = figure('Visible', 'off');
+subplot(3,3,1)
+imshow(img{anc_idx(30)});
+for i = 1:min(8,numel(neg{30})), subplot(3,3,i+1); imshow(img{neg{30}(i)}); end
+saveas(h, 'neg_anchor30.png');
+h = figure('Visible', 'off');
+subplot(3,3,1)
+imshow(img{anc_idx(100)});
+for i = 1:min(8,numel(neg{100})), subplot(3,3,i+1); imshow(img{neg{100}(i)}); end
+saveas(h, 'neg_anchor100.png');
